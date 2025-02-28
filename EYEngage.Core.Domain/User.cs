bcaad1f542c6233a7ffa05ca9bda93b7e9da638b
ghazi bouzid <ghazi.bouzid@esprit.tn>
2025-02-28 09:42:38 +0100

    adding User and Role Models

---
+++ EYEngage.Core.Domain/User.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EYEngage.Core.Domain
+{
+    public class User : IdentityUser<Guid>
+    {
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(100)]
+        public string LastName { get; set; }
+
+        [MaxLength(500)]
+        public string? ProfilePicture { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
